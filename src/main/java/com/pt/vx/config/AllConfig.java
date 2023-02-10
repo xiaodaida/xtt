9d0cff184a59683d393d803dda42c47673f1fd95
xiaodaida <94596999+xiaodaida@users.noreply.github.com>
2023-02-10 15:32:03 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -56,7 +56,7 @@ public class AllConfig {
 
     //额外类型消息
     public static final FunctionConfig open_xinGuan_info = new FunctionConfig(false,"#FF7F00"); //是否开启用户城市新冠信息查询
-    public static final FunctionConfig open_history_today = new FunctionConfig(true,"#FF7F00"); //是否开启历史上的今天
+    public static final FunctionConfig open_history_today = new FunctionConfig(false,"#FF7F00"); //是否开启历史上的今天
     public static final FunctionConfig open_qinghua = new FunctionConfig(true,"#FF7F00"); //是否开启情话一句
     public static final FunctionConfig open_dongman = new FunctionConfig(false,"#FF7F00"); //是否开启动漫台词(暂时无法使用了)
     public static final FunctionConfig open_tiangou = new FunctionConfig(false,"#FF7F00"); //是否开启舔狗日记
@@ -80,8 +80,8 @@ public class AllConfig {
                 "oGLUX6Of3rtjy8oFATCL98D-KfWc", //扫码关注你的测试号以后，测试平台会出现TA的微信号
                 "GihdxqHiT1HFd3oUtt5-UYNImMJCeVbEfCFSDRnAIBY", //要给这个人发送的模板ID
                 "小公主", //咋称呼这个人
-                "湖北省黄石市黄石港", //这个人的详细地址
-                "黄石", //这个人在的城市
+                "江苏省南京市玄武区", //这个人的详细地址
+                "南京", //这个人在的城市
                 new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
                 new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
                 new BirthDay(2020,7,8,true,true),
