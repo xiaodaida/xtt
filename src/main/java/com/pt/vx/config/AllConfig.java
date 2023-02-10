17a4a5ae472247db300415f361ae646212fe7e55
xiaodaida <94596999+xiaodaida@users.noreply.github.com>
2023-02-10 16:17:19 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -78,7 +78,7 @@ public class AllConfig {
         //new BirthDay()里面代表[年]、[月]、[日]、[是否是农历(true为农历、false为公历)]、[是否统计天数(true为统计，false为倒计时)]、[倒计时到0天提示信息(如果类型为统计可以不填)]
         userList.add(getUser(
                 "oGLUX6Of3rtjy8oFATCL98D-KfWc", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "3j2rjVgXkRnTzIQ2PC6TnVa01FPgZWpH24d0c8A5yGw", //要给这个人发送的模板ID
+                "na91SABWUKT8hilFIASJ5TzkByrfsQ8fq7REbbdg5HU", //要给这个人发送的模板ID
                 "小公主", //咋称呼这个人
                 "河南省开封市兰考县", //这个人的详细地址
                 "开封", //这个人在的城市
@@ -90,7 +90,7 @@ public class AllConfig {
 
         userList.add(getUser(
                 "oGLUX6Of3rtjy8oFATCL98D-KfWc", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "3j2rjVgXkRnTzIQ2PC6TnVa01FPgZWpH24d0c8A5yGw", //要给这个人发送的模板ID
+                "na91SABWUKT8hilFIASJ5TzkByrfsQ8fq7REbbdg5HU", //要给这个人发送的模板ID
                 "少爷", //咋称呼这个人
                 "湖北省黄石市黄石港区", //这个人的详细地址
                 "黄石", //这个人在的城市
